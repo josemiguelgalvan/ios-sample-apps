42260873fd86c1ea50e630a62813385883d15db1
Eric Vargas <evargas@ooyala.com>
2016-04-27 13:07:07 -0500

    [PBA-3635] Add sample app

---
+++ PlaybackLab/CocoaPodsTVSampleApp/CocoaPodsTVSampleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoaPodsTVSampleApp
+//
+//  Created by Eric Vargas on 4/27/16.
+//  Copyright © 2016 Ooyala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
